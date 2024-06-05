2e82041b760187a99c957fc46d6303e4926eda27
Petrenko Valeriya <mroom9195@gmail.com>
2024-06-05 19:19:25 +0300

    ex4

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
 int main() {
-	int x, y;
-	cout << "Enter two integers: ";
-	cin >> x >> y;
-	cout << "Before swap: x = " << x << ", y = " << y << endl;
-	swap(&x, &y);
-	cout << "After swap: x = " << x << ", y = " << y << endl;
+	int num1, num2;
+	int* p1, * p2;
+	char op;
+	int result;
+	cout << "Enter two numbers: ";
+	cin >> num1 >> num2;
+	p1 = &num1;
+	p2 = &num2;
+	cout << "Enter an operator (+, -, *, /): ";
+	cin >> op;
+	switch (op) {
+	case '+':
+		result = *p1 + *p2;
+		break;
+	case '-':
+		result = *p1 - *p2;
+		break;
+	case '*':
+		result = *p1 * *p2;
+		break;
+	case '/':
+		result = *p1 / *p2;
+		break;
+	}
+	cout << "Result: " << result << endl;
+	return 0;
 }
