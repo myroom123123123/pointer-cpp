334c1ad98a6551e8306093f6ffbe8c84c865c94f
Petrenko Valeriya <mroom9195@gmail.com>
2024-06-05 19:48:21 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
+int main() {
     const int SIZE = 5;
-    int * arr = new int[SIZE];;
-    int sum = 0;
+	int* arr = new int[SIZE];
     cout << "Enter " << SIZE << " integers:" << endl;
     for (int i = 0; i < SIZE; ++i) {
         cout << "Enter integer " << i + 1 << ": ";
         cin >> arr[i];
     }
-    int* ptr = arr;
-    for (int i = 0; i < SIZE; ++i) {
-        sum += *ptr;
-        ptr++;
+    int* ptr_start = arr;
+    int* ptr_end = arr + SIZE - 1;
+    while (ptr_start < ptr_end) {
+        int temp = *ptr_start;
+        *ptr_start = *ptr_end;
+        *ptr_end = temp;
+        ptr_start++;
+        ptr_end--;
     }
-    cout << "Sum of array elements: " << sum << endl;
-    delete[] arr;
+    cout << "Reversed array: ";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+	delete[] arr;
     return 0;
 }
+
