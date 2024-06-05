c3a9d74b3ccfe6f59c2f55c40b1866f32f428aa4
Petrenko Valeriya <mroom9195@gmail.com>
2024-06-05 19:30:14 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() { 
+int main() {
     const int SIZE = 5;
-    int arr[SIZE];
-    int sum = 0;
-    cout << "Enter " << SIZE << " integers:" << endl;
+    int arr1[SIZE];
+    int arr2[SIZE];
+    cout << "Enter " << SIZE << " integers for Array 1:" << endl;
     for (int i = 0; i < SIZE; ++i) {
         cout << "Enter integer " << i + 1 << ": ";
-        cin >> arr[i];
+        cin >> arr1[i];
     }
-    int* ptr = arr;
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
     for (int i = 0; i < SIZE; ++i) {
-        sum += *ptr;
-        ptr++;
+        *ptr2 = *ptr1;
+        ptr1++;
+        ptr2++;
     }
-    cout << "Sum of array elements: " << sum << endl;
+    cout << "Array 1: ";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+    cout << "Array 2 (copied from Array 1): ";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
